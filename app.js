fbf886dc1679d5430eedebd058070509f2eb6690
Mosa <Mosamsiza.29@gmail.com>
2025-05-13 18:14:02 +0200

    final home page and footer

--- app.js
+++ app.js
@@ -13,7 +13,9 @@ const eventsRoutes = require('./routes/eventsRoutes');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
+
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -1,15 +1,27 @@
 // routes/pageRoutes.js
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const events = require("../events.json");
 
-router.get('/', (req, res) => {
-    res.render('pages/home'); 
-  });
-  
-router.get('/about', (req, res) => res.render('pages/about'));
-router.get('/events', (req, res) => res.render('pages/events'));
-router.get('/contact', (req, res) => res.render('pages/contact'));
-router.get('/thankyou', (req, res) => res.render('pages/thankyou'));
+// Helper to parse date from the event's time string
+function parseDate(str) {
+  const datePart = str.split("From")[0].trim();
+  const cleaned = datePart.replace("h", ":").replace(/[^0-9/: ]/g, "");
+  return new Date(cleaned);
+}
+
+router.get("/", (req, res) => {
+  // Sort events by date descending and get latest 3
+  const sorted = events.sort((a, b) => parseDate(b.time) - parseDate(a.time));
+  const latestThree = sorted.slice(0, 3);
+
+  res.render("pages/home", { events: latestThree });
+});
+
+router.get("/about", (req, res) => res.render("pages/about"));
+router.get("/events", (req, res) => res.render("pages/events"));
+router.get("/contact", (req, res) => res.render("pages/contact"));
+router.get("/thankyou", (req, res) => res.render("pages/thankyou"));
 
 module.exports = router;
